cc12be776d7cff4a299ab89cc5e3b02b09deaaba
Alexander Sebhat <alexsanderephrem@gmail.com>
2025-04-29 17:29:00 +0400

    permodule errors partialy reolved ready for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
